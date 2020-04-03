3e0775ffab76b59e69bbf1c54c68c55ee9bc7c4d
HamelA <40329125+HamelA@users.noreply.github.com>
2020-04-03 00:31:44 -0700

    Initial Push
    Forgot to push throughout development. Everything is included.

---
+++ On-Rail Camera/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    
+    public void OpenScene()
+    {
+        SceneManager.LoadScene("Camera");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
